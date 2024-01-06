5b5bf272a3f9748b30ae3b7a77879cb1bfe45abf
Pedro Tricoli <pedrottlucas@gmail.com>
2024-01-06 17:52:54 -0300

    Create and update data-raw

---
+++ R/sample_address.R
@@ -0,0 +1,16 @@
+#' Sample Addresses from Campinas, São Paulo (SP)
+#'
+#' This dataset consists of a randomly selected sample of addresses located in Campinas, São Paulo (SP).
+#' It provides information about different random addresses within the city.
+#'
+#' @format A data frame with 99 rows and 5 columns:
+#' \describe{
+#'   \item{address}{Contains address information.}
+#'   \item{cep}{Represents the CEP (equivalent to Zip Code) corresponding to the address.}
+#'   \item{municipality}{Specifies the name of the municipality (Campinas).}
+#'   \item{state}{Two-letter abbreviation representing the state (SP - São Paulo).}
+#'   \item{country}{Specifies the name of the country (Brazil).}
+#'   ...
+#' }
+#' @source <https://github.com/PedroTL/geocoding-mini-curso-r/tree/main/Bancos%20de%20Dados>
+"sample_address"

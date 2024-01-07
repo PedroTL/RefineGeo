c9e7ab62cf74e4d1e3d7fed520b4a37f83a4243a
Pedro Tricoli <pedrottlucas@gmail.com>
2024-01-07 16:38:43 -0300

    Update test

--- tests/testthat/test-compare_cep.R
+++ tests/testthat/test-compare_cep.R
@@ -1,10 +1,8 @@
-# Sample Data
-df <- data.frame(
-  col_cep1 = c("12345677", "12345678", "12345", "12345", "12345"),
-  col_cep2 = c("12345678", "12345678", "54321", "123", "12345")
-)
-
 test_that("CEP Matching Test: Only eight-digit CEPs that are equal are considered a match", {
+
+  df <- data.frame(col_cep1 = c("12345677", "12345678", "12345", "12345", "12345"),
+                   col_cep2 = c("12345678", "12345678", "54321", "123", "12345"))
+
   expect_equal(compare_cep(df, "col_cep1", "col_cep2", strict_check = FALSE),
     c(0, 1, NA, NA, NA)
   )
@@ -12,6 +10,10 @@ test_that("CEP Matching Test: Only eight-digit CEPs that are equal are considere
 #> Test passed
 
 test_that("CEP Matching Test: Five and eight-digit CEPs that are equal are considered a match", {
+
+  df <- data.frame(col_cep1 = c("12345677", "12345678", "12345", "12345", "12345"),
+                   col_cep2 = c("12345678", "12345678", "54321", "123", "12345"))
+
   expect_equal(compare_cep(df, "col_cep1", "col_cep2", strict_check = TRUE),
     c(0, 1, 0, NA, 1)
   )
--- tests/testthat/test-extr_cep.R
+++ tests/testthat/test-extr_cep.R
@@ -1,11 +1,17 @@
 test_that("CEP Extraction Test: Only eight digits CEP pattern extraction works!", {
-  expect_equal(extr_cep(c("12345-678 Some Street, City, Country", "No ZIP code in this address", "Only subsector 12345")),
+
+  address <- c("12345-678 Some Street, City, Country", "No ZIP code in this address", "Only subsector 12345")
+
+  expect_equal(extr_cep(address),
                         c("12345678", NA, NA))
 })
 #> Tested passed
 
 test_that("CEP Extraction Test: Five and Eight digits CEP pattern extraction works!", {
-  expect_equal(extr_cep(c("12345-678 Some Street, City, Country", "No ZIP code in this address", "Only subsector 12345"), subsector_as_zip = TRUE),
+
+  address <- c("12345-678 Some Street, City, Country", "No ZIP code in this address", "Only subsector 12345")
+
+  expect_equal(extr_cep(address, subsector_as_zip = TRUE),
                c("12345678", NA, "12345"))
 })
 #> Tested passed
